ded602622c36b04eaed96fbde052684d89202124
jasonchg <36416641+jasonchg@users.noreply.github.com>
2020-07-21 01:25:16 +0800

    test
    test

---
+++ precache-manifest.5761f195d5ce5cd529839954bcdf1527.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f60899185a024bba69d518db7f7ab12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52179d9d92da2f7914d3",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "86168ddc02b6310f047e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "52179d9d92da2f7914d3",
+    "url": "/static/js/2.f5a82a1c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f5a82a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86168ddc02b6310f047e",
+    "url": "/static/js/main.06870299.chunk.js"
+  },
+  {
+    "revision": "10fa108189b7953c9d16",
+    "url": "/static/js/runtime-main.22151a8d.js"
+  },
+  {
+    "revision": "fcc8294d4fc09ccc04027e72c347579c",
+    "url": "/static/media/bg.fcc8294d.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
